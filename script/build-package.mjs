20175fc0d1587de0e64822c0bdb1a8fe839568ef
Jason Fabritz <github@bugbytes.com>
2022-10-31 16:54:22 -0500

    v0.31.1
    Expanded project to generate
    @bugbytes/hapi-mirror-proto along
    side @bugbytes/hapi-proto.  Two
    seperate npm packages were chosen
    becaue the HAPI protobuf contains
    namespace collisions.  Plus the mirror
    protobuf generates code that brings in
    an rxjs dependency.

--- script/build-package.mjs
+++ script/build-package.mjs
@@ -5,4 +5,5 @@ import url from 'url';
 const projDir = path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), '..');
 const libDir = path.join(projDir, 'lib');
 
-fs.copyFileSync(path.join(projDir, 'package.publish.json'), path.join(libDir, 'package.json'));
+fs.copyFileSync(path.join(projDir, 'package.publish.services.json'), path.join(libDir, 'services', 'package.json'));
+fs.copyFileSync(path.join(projDir, 'package.publish.mirror.json'), path.join(libDir, 'mirror', 'package.json'));
--- script/build-prepare.mjs
+++ script/build-prepare.mjs
@@ -3,19 +3,13 @@ import fs from 'fs';
 import url from 'url';
 
 const projDir = path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), '..');
-const protoMirrorDir = path.resolve(projDir, 'reference', 'hedera-protobufs', 'mirror');
-const protoServicesDir = path.resolve(projDir, 'reference', 'hedera-protobufs', 'services');
 const srcDir = path.join(projDir, 'src');
-const srcProto = path.join(srcDir, 'proto-src');
-const genProto = path.join(srcDir, 'proto');
 const libDir = path.join(projDir, 'lib');
 
 fs.rmSync(libDir, { recursive: true, force: true });
 fs.rmSync(srcDir, { recursive: true, force: true });
 fs.mkdirSync(srcDir);
-fs.mkdirSync(srcProto);
-fs.mkdirSync(genProto);
-fs.copyFileSync(path.join(protoMirrorDir, 'consensus_service.proto'), path.join(srcProto, 'consensus_service.proto'));
-for(const file of fs.readdirSync(protoServicesDir)) {
-  fs.copyFileSync(path.join(protoServicesDir, file), path.join(srcProto, file));
-}
+fs.mkdirSync(path.join(srcDir, 'services'));
+fs.mkdirSync(path.join(srcDir, 'services', 'services'));
+fs.mkdirSync(path.join(srcDir, 'mirror'));
+fs.mkdirSync(path.join(srcDir, 'mirror', 'mirror'));
